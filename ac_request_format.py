b0cedf5beea6ce294cfd9281b070c9b679ed6292
Andrew Maksimov <maksimov.andrew@gmail.com>
2019-09-20 15:07:28 +0300

    Class for active record format

---
+++ ac_request_format.py
@@ -0,0 +1,22 @@
+import sublime, sublime_plugin
+
+class AcRequestFormatCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    region = self.view.sel()[0]
+    if not region.empty():
+      blocks             = self.view.substr(region).split('.')
+      startrow, startcol = self.view.rowcol(region.begin())
+      new_line           = ''
+      first_line         = 0
+
+      for block in blocks:
+        if blocks.index(block) == 0:
+          new_line += block + '.'
+        elif blocks.index(block) == 1:
+          new_line  += block + '.\n'
+          first_line = len(new_line) + startcol - 2
+        else:
+          spaces    = [" " for x in range(first_line)]
+          new_line += ''.join(spaces) + '.' + block + '\n'
+
+      self.view.replace(edit, region, new_line)
