01e31a367f8c75da865a5e3f287ac6dad5d26e09
Andrew Maksimov <maksimov.andrew@gmail.com>
2019-10-11 15:57:15 +0300

    excelude not ac dots from split

--- ac_request_format.py
+++ ac_request_format.py
@@ -2,6 +2,7 @@ import sublime, sublime_plugin
 
 class AcRequestFormatCommand(sublime_plugin.TextCommand):
   def run(self, edit):
+
     region = self.view.sel()[0]
     if not region.empty():
       blocks             = self.view.substr(region).split('.')
@@ -11,12 +12,15 @@ class AcRequestFormatCommand(sublime_plugin.TextCommand):
 
       for block in blocks:
         if blocks.index(block) == 0:
-          new_line += block + '.'
+          new_line  += block + '.'
           first_line = len(block) + startcol
         elif blocks.index(block) == 1:
-          new_line  += block + '\n'
+          new_line  += block
         else:
           spaces    = [" " for x in range(first_line)]
-          new_line += ''.join(spaces) + '.' + block + '\n'
+          if 'joins' in block or 'where' in block or 'order' in block or 'limit' in block:
+            new_line += '\n' + ''.join(spaces) + '.' + block
+          else:
+            new_line += '.' + block
 
       self.view.replace(edit, region, new_line)
